7b94018f626af0eb4adf6ee4cd717a7af09c238f
Joe Pollock <cjosephpollock@gmail.com>
2015-11-24 14:49:21 -0800

    set up angular file

--- js/app.js
+++ js/app.js
@@ -5,36 +5,59 @@ angular.module('rateMyClass', ['ui.router','firebase'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/");
-
-
   $stateProvider
     .state('home', {
       url: "/",
-      templateUrl: "partials/home.html"
+      templateUrl: "partials/home.html",
+      controller:'homeCtrl'
     })
     .state('search', {
       url: "/search",
       templateUrl: "partials/search.html"
+      controller:'searchCtrl'
     })
     .state('about', {
       url: "/about",
       templateUrl: "partials/about.html"
+      controller:'aboutCtrl'
     })
     .state('contact', {
       url: "/contact",
       templateUrl: "partials/contact.html"
+      controller:'contactCtrl'
     })
     .state('addClass', {
       url: "/addClass",
       templateUrl: "partials/addclass.html"
+      controller:'addclassCtrl'
     })
+
+  $urlRouterProvider.otherwise("/");
 
 })
 
 .controller('homeCtrl', ['$scope', function($scope) {
     var ref = new Firebase("https://ratemyclass.firebaseio.com/");
+    console.log("home controller!");
 }]);
 
+.controller('searchCtrl', ['$scope', function($scope) {
+    var ref = new Firebase("https://ratemyclass.firebaseio.com/");
+    console.log("home controller!");
+}]);
+
+.controller('aboutCtrl', ['$scope', function($scope) {
+    var ref = new Firebase("https://ratemyclass.firebaseio.com/");
+    console.log("home controller!");
+}]);
+
+.controller('contactCtrl', ['$scope', function($scope) {
+    var ref = new Firebase("https://ratemyclass.firebaseio.com/");
+    console.log("home controller!");
+}]);
+
+.controller('addclassCtrl', ['$scope', function($scope) {
+    var ref = new Firebase("https://ratemyclass.firebaseio.com/");
+    console.log("home controller!");
+}]);
 
