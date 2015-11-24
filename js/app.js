5d33fe7536d72aa614375c5ee4a89305bde352e1
Joe Pollock <cjosephpollock@gmail.com>
2015-11-24 14:42:18 -0800

    added all partials for different states

--- js/app.js
+++ js/app.js
@@ -14,6 +14,22 @@ angular.module('rateMyClass', ['ui.router','firebase'])
       url: "/",
       templateUrl: "partials/home.html"
     })
+    .state('search', {
+      url: "/search",
+      templateUrl: "partials/search.html"
+    })
+    .state('about', {
+      url: "/about",
+      templateUrl: "partials/about.html"
+    })
+    .state('contact', {
+      url: "/contact",
+      templateUrl: "partials/contact.html"
+    })
+    .state('addClass', {
+      url: "/addClass",
+      templateUrl: "partials/addclass.html"
+    })
 
 })
 
