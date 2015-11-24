bcac95759293c0deae88b05ffabe14de259d467f
Joe Pollock <cjosephpollock@gmail.com>
2015-11-24 14:37:10 -0800

    partials working

--- js/app.js
+++ js/app.js
@@ -4,34 +4,17 @@ angular.module('rateMyClass', ['ui.router','firebase'])
 
 
 .config(function($stateProvider, $urlRouterProvider) {
-  // //
+
   // // For any unmatched url, redirect to /state1
-  // $urlRouterProvider.otherwise("/state1");
-  // //
-  // // Now set up the states
-  // $stateProvider
-  //   .state('state1', {
-  //     url: "/state1",
-  //     templateUrl: "partials/state1.html"
-  //   })
-  //   .state('state1.list', {
-  //     url: "/list",
-  //     templateUrl: "partials/state1.list.html",
-  //     controller: function($scope) {
-  //       $scope.items = ["A", "List", "Of", "Items"];
-  //     }
-  //   })
-  //   .state('state2', {
-  //     url: "/state2",
-  //     templateUrl: "partials/state2.html"
-  //   })
-  //   .state('state2.list', {
-  //     url: "/list",
-  //     templateUrl: "partials/state2.list.html",
-  //     controller: function($scope) {
-  //       $scope.things = ["A", "Set", "Of", "Things"];
-  //     }
-  //   });
+  $urlRouterProvider.otherwise("/");
+
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "partials/home.html"
+    })
+
 })
 
 .controller('homeCtrl', ['$scope', function($scope) {
